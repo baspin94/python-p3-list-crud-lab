cac94bea03343de91b685de7cc264ccfc98a1e84
Bianca Aspin <bianca@mj94.net>
2023-02-21 17:40:40 -0800

    Complete lab.

--- lib/list_crud.py
+++ lib/list_crud.py
@@ -1,26 +1,36 @@
+""" import ipdb """
+
 def create_an_empty_list():
-    return None
+    empty_list = []
+    return empty_list
 
 def create_a_list():
-    return None
+    new_list = ["apple", "banana", "cucumber", "dragonfruit"]
+    return new_list
 
-def add_element_to_end_of_list(l, element):
-    return None
+def add_element_to_end_of_list(list, element):
+    list.append(element)
+    return list
 
-def add_element_to_start_of_list(l, element):
-    return None
+def add_element_to_start_of_list(list, element):
+    list.insert(0, element)
+    return list
 
-def remove_element_from_end_of_list(l):
-    return None
+def remove_element_from_end_of_list(list):
+    list.pop()
+    return list
 
-def remove_element_from_start_of_list(l):
-    return None
+def remove_element_from_start_of_list(list):
+    del(list[0])
+    return list
 
-def retrieve_first_element_from_list(l):
-    return None
+def retrieve_first_element_from_list(list):
+    return list[0]
 
-def retrieve_element_from_index(l, index):
-    return None
+def retrieve_element_from_index(list, index):
+    return list[index]
 
-def retrieve_last_element_from_list(l):
-    return None
+def retrieve_last_element_from_list(list):
+    return list[-1]
+
+""" ipdb.set_trace() """
